23eba2ca221ca309b75801d0d6df44838b774440
Igor Sales <igorsales.fs@gmail.com>
2020-10-09 23:59:12 -0300

    update ted 5

--- quinto-ted/src/main/java/br/com/igorsales/quintoted/models/Banco.java
+++ quinto-ted/src/main/java/br/com/igorsales/quintoted/models/Banco.java
@@ -8,6 +8,37 @@ public class Banco {
     private String razaoSocial;
     private  String CNPJ;
 
+    public Banco() {
+    }
+
+    public Banco(String razaoSocial, String CNPJ) {
+        this.razaoSocial = razaoSocial;
+        this.CNPJ = CNPJ;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    @Override
+    public String toString() {
+        return "Banco{" +
+                "razaoSocial='" + razaoSocial + '\'' +
+                ", CNPJ='" + CNPJ + '\'' +
+                '}';
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
 
     public Conta getContaMaxSaldo(ArrayList<Conta> contas ){
         ArrayList<Double> values = new ArrayList<>();
